aebde96e107c04cc722d96263d6a79b28375805e
Vanessa Pasquet <pasquet.vanessa@gmail.com>
2013-07-10 16:58:14 +0200

    distinct forms for edit and new recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,5 @@ class Recipe < ActiveRecord::Base
   has_many :tags, :through => :recipes_tags
   has_many :recipes_tags
 
-  attr_accessible :body, :title, :user_attributes, :image_url
+  attr_accessible :body, :title, :user_attributes, :image_url, :ingredient_list, :number_of_person, :prep_time
 end
