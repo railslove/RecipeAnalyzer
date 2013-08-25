80903f5611fdc839651b73cf40e2c201f01baa76
Jan Kus <jan@railslove.com>
2013-08-25 18:14:10 +0200

    typo

--- db/migrate/20130607151641_add_tag_id_to_ingredient.rb
+++ db/migrate/20130607151641_add_tag_id_to_ingredient.rb
@@ -1,6 +1,6 @@
 class AddTagIdToIngredient < ActiveRecord::Migration
   def change
-    add_column :ingredients, :tags, :integers
+    add_column :ingredients, :tags, :integer
     add_index :ingredients, :tags
   end
 end
